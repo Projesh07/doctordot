78139ca93dfc0a5431430ad86cefe2509987247c
projesh <projesh>
2017-12-01 14:09:54 +0600

    Post template added

--- doctordot/posts/models.py
+++ doctordot/posts/models.py
@@ -19,7 +19,7 @@ class Post(models.Model):
 	def __str__(self):
 		return self.title
 
-	def get_absolute_url():
+	def get_absolute_url(self):
 		return reverse("posts:detail",kwargs={"id":self.id})
 		#return "/posts/%s/"%(self.id)	
 
--- doctordot/posts/urls.py
+++ doctordot/posts/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_index),
+    url(r'^$', views.post_index,name="list"),
     url(r'^create/$', views.post_create),
     url(r'^(?P<id>\d+)/$', views.post_details, name="detail"),
-    url(r'^delete/$', views.post_delete),
-    url(r'^update/$', views.post_update),
+    url(r'^(?P<id>\d+)/delete/$', views.post_delete),
+    url(r'^(?P<id>\d+)/edit/$', views.post_update,name="update"),
     #url(r'^posts/$', views.post_index),
 ]
